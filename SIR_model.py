3dc771fe1265ac8f0282edeadeb115c529e28d60
Tiig3r <leplusbeauswing@gmail.com>
2021-04-06 08:34:43 +0200

    Ajout de certains commentaires

--- SIR_model.py
+++ SIR_model.py
@@ -8,14 +8,19 @@ I0 = 1
 R0 = 0
 S0 = POPULATION - I0 - R0
 
+# Vecteur représentant les 3 conditions initiales.
 y0 = S0, I0, R0
 
 # Les taux
+
+# BETA correspond au taux d'infection / probabilité d'être infecté après avoir été en contact avec un individu infecté
 BETA = 0.8
+
+# GAMMA correspond à la probabilité de ne plus pouvoir transmettre le virus
 GAMMA = 0.05
 
 
-# Les variations des grandeurs S(t), I(t) et R(t)
+# Les variations des grandeurs S(t), I(t) et R(t) => Dérivées ds/dt, di/dt, dr/dt
 def variation(y, t, beta, gamma):
     s, i, r = y
     dsdt = - beta * s * i
@@ -25,10 +30,14 @@ def variation(y, t, beta, gamma):
 
 
 # Creation des tableaux
+
+# Tableau représentant l'échelle du temps
 t = np.linspace(0, 50)
 
-# Intégration du système
+# Intégration du système par rapport au temps
 res = odeint(variation, y0, t, args=(BETA, GAMMA))
+
+# Affichage des courbes
 
 plt.figure("Modèle SIR")
 plt.title("Evolution de la taille des 3 catégories de personnes au cours du temps")
