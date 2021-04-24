45afe31827eb43a3a0f514472ea36f116239962f
Tiig3r <leplusbeauswing@gmail.com>
2021-04-24 12:41:20 +0200

    Mise à jour suite à la création du fichier consts.py

--- SIR_model.py
+++ SIR_model.py
@@ -1,20 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
-
-# Conditions initiales sur la population
-POPULATION = 1000
-I0 = 1
-R0 = 0
-S0 = POPULATION - I0 - R0
-
-# Les taux
-
-# BETA correspond au taux d'infection / probabilité d'être infecté après avoir été en contact avec un individu infecté
-BETA = 0.8
-
-# GAMMA correspond à la probabilité de ne plus pouvoir transmettre le virus
-GAMMA = 0.05
+import consts
 
 
 # Les variations des grandeurs S(t), I(t) et R(t) => Dérivées ds/dt, di/dt, dr/dt
@@ -34,7 +21,7 @@ t = np.linspace(0, 90, 90)
 print(t)
 
 # Intégration du système
-sir = odeint(variation, [S0, I0, R0], t, args=(BETA, GAMMA, POPULATION))
+sir = odeint(variation, [consts.S0, consts.I0, consts.R0], t, args=(consts.BETA, consts.GAMMA, consts.POPULATION))
 print(sir)
 
 # Affichage des courbes
