e25a9461d92809a85a71a8956c78775a8628f877
Tiig3r <leplusbeauswing@gmail.com>
2021-06-01 13:01:23 +0200

    Premiers tests sur la BDD

--- server/main.py
+++ server/main.py
@@ -1,5 +1,21 @@
 import server.database
 
-connection_db = server.database.connection("127.0.0.1", "root", "", "tipe")
-print(type(connection_db))
-print(connection_db)
+db = None
+try:
+    db = server.database.Database("127.0.0.1", "root", "", "tipe")
+    print("Connecté à la BDD")
+except:
+    print("Une erreur est survenue lors de la connexion à la BDD.")
+
+
+# Quelques tests
+if db is not None:
+    # SELECT
+    print(db.get_cursor())
+    test = db.query("SELECT * FROM contamines")
+    print(test)
+    print(db.get_cursor())
+
+    # INSERT
+    db.insert("INSERT INTO contamines (nom, prenom, age, nationalite) VALUES ('Jean', 'Pascal', 36, 'Française')")
+    print(db.get_cursor())
