f57a7cfc77df32004fdb025363859f0375e80558
Tiig3r <leplusbeauswing@gmail.com>
2021-06-12 16:34:38 +0200

    Ajout d'un logger

---
+++ server/logger.py
@@ -0,0 +1,22 @@
+def info(text=""):
+    """
+    Affiche une information
+    :param text:
+    """
+    print("[INFO] " + text)
+
+
+def error(text=""):
+    """
+    Affiche une erreur
+    :param text:
+    """
+    print("[ERROR] " + text)
+
+
+def line(text=""):
+    """
+    Affiche une ligne sans tag
+    :param text:
+    """
+    print(text)
